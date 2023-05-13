e310fb78a9e1944c7aa46ba938da4151b31fb310
Cole Peterson <rcolepeterson@gmail.com>
2023-05-13 06:52:57 -0700

    new examples and some comments

--- app/components/Cole.js
+++ app/components/Cole.js
@@ -1,13 +1,22 @@
 "use client";
 import React from "react";
+
+// client side componenet. it can have state. see the use client at the top.
+
 const Cole = ({ label }) => {
+  // state
   const [mycount, setCount] = React.useState(20);
   const [myCaro, setMyCaro] = React.useState("no data");
   const [myHello, setMyHello] = React.useState("no data");
+  //more complex state.
+  const [state, setState] = React.useState({ start: true, people: [1, 2, 3] });
+
   const updateCount = () => {
     console.log("updateCount");
+    // get a random number between 1 and 1000
+    let random = Math.floor(Math.random() * 1000) + 1;
     //do some stuff
-    setCount(300);
+    setCount(random);
   };
 
   //function that fetches data from api/caro and saves it on the state.
@@ -27,20 +36,39 @@ const Cole = ({ label }) => {
 
   return (
     <div className="flex flex-col">
-      <h1 className="uppercase">{label}</h1>
+      <h1 className="uppercase">label: {label}</h1>
+
+      {/* display the count which is stored on the state. */}
       <p>{mycount}</p>
 
-      <button className="btn bg-slate-600 border-10 mb-6" onClick={updateCount}>
-        <h5>Update</h5>
+      <button
+        className="btn bg-slate-600 mb-6 rounded-full p-2"
+        onClick={updateCount}
+      >
+        <h5>Click me to Update</h5>
       </button>
-      <button className="btn bg-blue-500 border-10" onClick={fetchData}>
-        <h5>Fetch Caro data</h5>
+      <button
+        className="btn bg-orange-500 p-2 rounded-full"
+        onClick={fetchData}
+      >
+        <h5>Click me to Fetch Caro data</h5>
       </button>
       <p>CaroData: {myCaro}</p>
-      <button className="btn bg-blue-500 border-10" onClick={fetchHelloData}>
-        <h5>Fetch Hello data</h5>
+      <button
+        className="btn bg-blue-500 rounded-3xl p-4"
+        onClick={fetchHelloData}
+      >
+        <h5>Click me to Fetch Hello data</h5>
       </button>
       <p>HelloData: {myHello}</p>
+      {/* display the state.people using array.map and use a key */}
+      {state.people.map((person, index) => {
+        return (
+          <div className="w-[60px] h-[60px] bg-pink-500 m-1" key={index}>
+            <p>{person}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Cole from "./components/Cole";
 
 export default async function Home() {
   let myStyle = "text-green-500 mt-6";
-  // This file is server side rendered. So you can just make requests whenever you want.
+  // Hi Caro. This file is server side rendered. So you can just make requests whenever you want.
   // Get some data from the api/hello using async await.
   // You cant use state though. We are on the server. So maybe forget for now.
 
@@ -10,9 +10,14 @@ export default async function Home() {
     const res = await fetch(`http://localhost:3000/api/hello`);
     return res.json();
   }
+
+  // Get some data from the api/hello using async await.
   let data = await getData();
+
+  // return what is going to be rendered to the page / screen / website.
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* this is an example component */}
       <Cole label="scott" />
 
       <h2 className="text-2xl">
