9190f66f2de26e74d92e9d8b8ac5f85f7c0e8f73
Cole Peterson <rcolepeterson@gmail.com>
2023-05-15 07:24:08 -0700

    about page for caro

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+const About = () => {
+  // this is a server side rendered component.
+  return (
+    <div>
+      <h1>About</h1>
+      <p> Hi caro!!!!</p>
+    </div>
+  );
+};
+
+export default About;
