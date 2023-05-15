c4d92ac9ec9f1bf68176fd7cc3ee9a9c28b92c2f
Cole Peterson <rcolepeterson@gmail.com>
2023-05-15 10:16:38 -0700

    ssr data fetch to about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,14 @@
-const About = () => {
-  // this is a server side rendered component.
+// this is a server side rendered component.
+
+const About = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  let data = await res.json();
+  console.log(data);
   return (
     <div>
       <h1>About</h1>
       <p> Hi caro!!!!</p>
+      <p>{data.title}</p>
     </div>
   );
 };
